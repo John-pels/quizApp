e15a4bb3a12a3bd859b47b54f2d87507adc3f859
john-pels <oluseyiajeigbe@yahoo.com>
2019-06-03 11:47:05 +0100

    styling the  game page

--- js/game.js
+++ js/game.js
@@ -43,7 +43,6 @@ startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-    console.log(availableQuestions);
     getNewQuestion();
 };
 getNewQuestion = () => {
@@ -69,8 +68,20 @@ choices.forEach(choice => {
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'];
-        console.log(selectedAnswer);
-        getNewQuestion();
+
+        // const classToApply = 'incorrect';
+        // if (selectedAnswer == currentQuestion.answer) {
+        //     classToApply = 'correct';
+        // }
+        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+
     });
 });
 startGame();
