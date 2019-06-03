60eef69e381fefb2814f73bf975807fb0c57510f
john-pels <oluseyiajeigbe@yahoo.com>
2019-06-03 16:09:30 +0100

    Created a spinning loader

--- js/game.js
+++ js/game.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -32,6 +34,7 @@ fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=m
             return formattedQuestion;
         });
 
+
         startGame();
     })
     .catch(err => {
@@ -48,6 +51,8 @@ startGame = () => {
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
 };
 getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
